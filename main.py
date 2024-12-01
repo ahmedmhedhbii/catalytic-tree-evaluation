1ad1c3d0f245489c67d2557d4f6af22ee7e089b2
Ahmed Mhedhbi <127897977+ahmedmhedhbii@users.noreply.github.com>
2024-12-01 18:17:06 -0500

    Updated file

--- main.py
+++ main.py
@@ -4,39 +4,26 @@ import random
 import copy
 
 
-def initialize_field(k):
-    field_order = 1 << math.ceil(math.log2(2 * math.ceil(math.log2(k)) + 2))
-    field = galois.GF(field_order)
-    return field, field_order
+def q_u_i(y, z, f_u, i):
+    def e_poly(y, beta):
+        res = field(1)
+        for yi, betai in zip(y, beta):
+            res *= field(1) - yi + (2 * yi - field(1)) * betai
+        return res
 
-
-def find_primitive_root(GF):
-    return GF.primitive_element
-
-
-def e_poly(y, beta, field):
-    res = field(1)
-    for yi, betai in zip(y, beta):
-        res *= field(1) - yi + (field(2) * yi - field(1)) * betai
-    return res
-
-
-def q_u_i(y, z, f_u, field, i):
     res = field(0)
-    l = k.bit_length()
     for alpha in range(k):
         for beta in range(k):
             for gamma in range(k):
-                alpha_bits = [int(bit) for bit in f'{alpha:0{l}b}'[::-1]]
-                beta_bits = [int(bit) for bit in f'{beta:0{l}b}'[::-1]]
-                gamma_bits = [int(bit) for bit in f'{gamma:0{l}b}'[::-1]]
-                if alpha_bits[i] == 1 and f_u[beta][gamma] == alpha:
-                    res += e_poly(y, beta_bits, field) * e_poly(z, gamma_bits, field)
+                alpha_bits = [field(int(bit)) for bit in f'{alpha:0{log_k}b}'[::-1]]
+                beta_bits = [field(int(bit)) for bit in f'{beta:0{log_k}b}'[::-1]]
+                gamma_bits = [field(int(bit)) for bit in f'{gamma:0{log_k}b}'[::-1]]
+                if alpha_bits[i] == field(1) and f_u[beta][gamma] == alpha:
+                    res += e_poly(y, beta_bits) * e_poly(z, gamma_bits)
     return res
 
 
-
-def field_sum(bits, field):
+def field_sum(bits):
     total = field(0)
     for i, bit in enumerate(bits):
         total += bit * (field(2) ** i)
@@ -44,7 +31,6 @@ def field_sum(bits, field):
 
 
 def recursive_clean(node, level, h):
-    log_k = math.ceil(math.log2(k))
     if level == h:
         bits = [field(int(bit)) for bit in f'{tree[node]:0{log_k}b}'[::-1]]
         for i, bit in enumerate(bits):
@@ -63,7 +49,7 @@ def recursive_clean(node, level, h):
         recursive_clean(right_child, level + 1, h)
 
         for i in range(log_k):
-            registers[node][i] -= q_u_i(registers[left_child], registers[right_child], tree[node], field, i)
+            registers[node][i] -= q_u_i(registers[left_child], registers[right_child], tree[node], i)
 
         recursive_clean_inverse(left_child, level + 1, h)
         recursive_clean_inverse(right_child, level + 1, h)
@@ -74,7 +60,6 @@ def recursive_clean(node, level, h):
 
 
 def recursive_clean_inverse(node, level, h):
-    log_k = math.ceil(math.log2(k))
     if level == h:
         bits = [field(int(bit)) for bit in f'{tree[node]:0{log_k}b}'[::-1]]
         for i, bit in enumerate(bits):
@@ -89,50 +74,72 @@ def recursive_clean_inverse(node, level, h):
             for i in range(log_k):
                 registers[c][i] *= omega ** j
 
-        recursive_clean(left_child, level + 1, h)
-        recursive_clean(right_child, level + 1, h)
-
-        for i in range(log_k):
-            registers[node][i] += q_u_i(registers[left_child], registers[right_child], tree[node], field, i)
-
         recursive_clean_inverse(left_child, level + 1, h)
         recursive_clean_inverse(right_child, level + 1, h)
+
+        for i in range(log_k):
+            registers[node][i] += q_u_i(registers[left_child], registers[right_child], tree[node], i)
+
+        recursive_clean(left_child, level + 1, h)
+        recursive_clean(right_child, level + 1, h)
 
         for c in [left_child, right_child]:
             for i in range(log_k):
                 registers[c][i] /= omega ** j
 
 
-
 def clean_computation(h):
     initial_registers = copy.deepcopy(registers[0])
     recursive_clean(0, 0, h)
-    final_result = field_sum(registers[0], field) - field_sum(initial_registers, field)
+    final_result = field_sum(registers[0]) - field_sum(initial_registers)
     return final_result, registers
 
 
-def initialize_tree_and_catalyst():
-    h = 1  # int(input("Enter tree height: "))
-    k = 10  # int(input("Enter alphabet size: "))
-    num_nodes = (1 << (h + 1)) - 1
+def initialize_field(k):
+    field_order = 1 << math.ceil(math.log2(2 * math.ceil(math.log2(k)) + 2))
+    field = galois.GF(field_order)
+    return field, field_order
 
-    tree_values = [1] * (1 << h) # to be defined
-    tree_functions = [[list(range(k)) for _ in range(k)] for _ in range((1 << h) - 1)]  # to be defined
+
+def initialize_tree_and_catalyst():
+    h = int(input("Enter the height of the tree: "))
+    k = int(input("Enter the size of the alphabet: "))
+    num_nodes = (1 << (h + 1)) - 1
+    function = [[int(input(f"Enter the value for function at row {r}, column {c}: ")) for c in range(k)]
+                for r in range(k)]
+    tree_functions = [function for _ in range((1 << h) - 1)]
+
+    tree_values = [int(input(f"Enter the value of leaf node {i}: ")) for i in range(1 << h)]
     tree = tree_functions + tree_values
 
-    field, m = initialize_field(k)
+    log_k = math.ceil(math.log2(k + 1))
 
-    registers = [[field(random.randint(0, field.order - 1)) for _ in range(math.ceil(math.log2(k)))] for _ in
-                range(num_nodes)]
+    field, field_order = initialize_field(k)
+    registers = [[field(random.randint(0, field.order - 1)) for _ in range(log_k)] for _ in
+                 range(num_nodes)]
 
-    return h, k, tree, registers, m, field
+    return h, k, tree, registers, field, field_order - 1
+
+
+def the_not_so_cool_algorithm(node, level):
+    if level == height:
+        return tree[node]
+
+    left = the_not_so_cool_algorithm(2 * node + 1, level + 1)
+    right = the_not_so_cool_algorithm(2 * node + 2, level + 1)
+
+    return tree[node][left][right]
 
 
 if __name__ == "__main__":
-    height, k, tree, registers, m, field = initialize_tree_and_catalyst()
-    omega = find_primitive_root(field)
+    height, k, tree, registers, field, m = initialize_tree_and_catalyst()
+    log_k = math.ceil(math.log2(k + 1))
+    omega = field.primitive_element
     registers_copy = copy.deepcopy(registers)
     result, final_registers = clean_computation(height)
+
+    print("*" * 80)
+    assert result == the_not_so_cool_algorithm(0, 0)
     print(f"Tree evaluation result: {result}")
-    print(f"Initial registers: {registers_copy}")
-    print(f"Final registers: {final_registers}")
+    print(f"Initial registers:\n{registers_copy}")
+    print(f"Final registers:\n{final_registers}")
